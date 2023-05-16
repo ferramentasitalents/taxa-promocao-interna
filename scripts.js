3127c1a145a854282a36ab6046884e17ee4a0cb7
ferramentasitalents <133061937+ferramentasitalents@users.noreply.github.com>
2023-05-16 17:23:26 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,7 @@
+function calculatePromotionRate() {
+  const numPromovidos = parseFloat(document.getElementById("num-promovidos").value);
+  const numFuncionarios = parseFloat(document.getElementById("num-funcionarios").value);
+  const rate = (numPromovidos / numFuncionarios) * 100;
+  document.getElementById("result").innerHTML = `Taxa de Promoção Interna: ${rate.toFixed(2)}%`;
+}
 
